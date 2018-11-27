1f18c9a88131c947404eab3584cf19c92ecadb54
Zachjrivera <Zachjrivera@gmail.com>
2018-11-27 07:13:28 -0500

    will not post

---
+++ src/app/app.module.ts
@@ -0,0 +1,24 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+import { ReactiveFormsModule } from '@angular/forms';
+import { HttpModule } from '@angular/http';
+
+import { AppComponent } from './app.component';
+import { ArticleComponent } from './article.component';
+import { ArticleService } from './article.service';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    ArticleComponent
+  ],
+  imports: [
+    BrowserModule,
+    HttpModule,
+	ReactiveFormsModule,
+
+  ],
+  providers: [  ArticleService ],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
